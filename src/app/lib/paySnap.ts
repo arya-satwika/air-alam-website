6332d7b7b950ee3f10fa9e2b2e451919b9b5ebdf
arya-satwika <aryastwk95@gmail.com>
2026-05-21 13:07:36 +0700

    payment pake snap

--- src/app/lib/paySnap.ts
+++ src/app/lib/paySnap.ts
@@ -5,22 +5,30 @@ export type transactionDetails = {
 }
 
 const midtransClient = require('midtrans-client');
-const core = new midtransClient.CoreApi({
+const snap = new midtransClient.Snap({
     isProduction : false,
-    serverKey : process.env.MIDTRANS_SERVER_KEY,
-    clientKey : process.env.MIDTRANS_CLIENT_KEY
+    serverKey : process.env.MIDTRANS_SERVER_KEY
 });
 
 // const serverKey = process.env.MIDTRANS_SERVER_KEY || '';
 // export const authHeader = Buffer.from(serverKey, 'utf-8').toString('base64');
 
-export async function payQris(transactionDetails: transactionDetails): Promise<string> {
+export async function paySnap(transactionDetails: transactionDetails): Promise<string> {
 
     let parameter = {
         "payment_type": "gopay",
         "transaction_details": {
             "gross_amount": transactionDetails.gross_amount,
             "order_id": transactionDetails.order_id
+        },
+        "credit-card":{
+            "secure" : true
+        },
+        "customer_details": {
+            "first_name": "budi",
+            "last_name": "pratama",
+            "email": "budi.pra@example.com",
+            "phone": "08111222333"
         }
     }
     // core.charge(parameter)
@@ -34,24 +42,14 @@ export async function payQris(transactionDetails: transactionDetails): Promise<s
     //     return '';
     // });
     try {
-        const response = await core.charge(parameter);
-        const qrisUrl = response.actions.find((action: { name: string }) => action.name === 'generate-qr-code')?.url;
-        console.log('QRIS URL:', qrisUrl);
-        return qrisUrl || '';
+        const response = await snap.createTransaction(parameter);
+        const redirectUrl = response.redirect_url;
+        const snapToken = response.token;
+        console.log('Redirect URL:', redirectUrl);
+        return redirectUrl || '';
 
     } catch (error) {
         console.error('Error charging transaction:', error);
         return '';
     }
-}
-
-export async function payTransfer(transactionDetails: transactionDetails): Promise<string> {
-    let parameter = {
-        "payment_type": "transfer.",
-        "transaction_details": {
-            "gross_amount": transactionDetails.gross_amount,
-            "order_id": transactionDetails.order_id
-        }
-    }   
-    return '';
 }
--- src/app/pay/page.tsx
+++ src/app/pay/page.tsx
@@ -1,13 +1,14 @@
 'use client'
 import { payQris } from "@/app/lib/action";
+import { paySnap } from "@/app/lib/paySnap";
 export default function Pay() {
     async function handlePay() {
         console.log('Initiating payment...');
         const transactionDetails = {
             gross_amount: 10000,
-            order_id: "222dorder-0000"
+            order_id: "222dordder-0000"
         };
-        const qrisUrl = await payQris(transactionDetails);
+        const qrisUrl = await paySnap(transactionDetails);
         console.log('QRIS URL:', qrisUrl);
     }
     return (
