3dec1a563266f5c6bae1fd1edeb4771cc6f0ec95
arya-satwika <aryastwk95@gmail.com>
2026-05-21 13:44:29 +0700

    contoh isi cart

--- src/app/pay/page.tsx
+++ src/app/pay/page.tsx
@@ -1,20 +1,28 @@
 'use client'
 import { payQris } from "@/app/lib/action";
 import { paySnap } from "@/app/lib/paySnap";
+import { redirect } from "next/navigation";
 export default function Pay() {
-    async function handlePay() {
+    async function handlePay(formData: FormData) {
         console.log('Initiating payment...');
+        const orderId = formData.get('order_id') as string;
+        const grossAmount = parseInt(formData.get('gross_amount') as string);
         const transactionDetails = {
-            gross_amount: 10000,
-            order_id: "222dordder-0000"
+            gross_amount: grossAmount,
+            order_id: orderId
         };
-        const qrisUrl = await paySnap(transactionDetails);
-        console.log('QRIS URL:', qrisUrl);
+        const redirectUrl = await paySnap(transactionDetails);
+        redirect(redirectUrl)
+        console.log('Redirect URL:', redirectUrl);
     }
     return (
         <div>   
+            <form action={handlePay}>
+                <input type="text" name="order_id" placeholder="Order ID" />
+                <input type="text" name="gross_amount" placeholder="Gross Amount" />
+                <button type="submit">Pay with QRIS</button>
+            </form>
             <h1 className="font-bold text-4xl">Pay</h1>
-            <button onClick={handlePay}>Pay with QRIS</button>
         </div>
     )
 }
