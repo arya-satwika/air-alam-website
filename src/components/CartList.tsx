81c5cbf2f682fd347474358df28e7578a0b3d29b
arya-satwika <aryastwk95@gmail.com>
2026-05-23 14:39:40 +0700

    add items array

--- src/components/CartList.tsx
+++ src/components/CartList.tsx
@@ -5,17 +5,7 @@ import CartItems from "./CartItems"
 import { getCart } from "@/app/lib/cart"
 
 export default function CartList() {
-    const cartItems = getCart() //JSON.parse(localStorage.getItem('cart') || '[]')
-//     const cartItems = [
-//     {
-//         id: 3,
-//         quantity: 2
-//     },
-//     {
-//         id: 2,
-//         quantity: 1
-//     }
-// ]
+
     return (
         <div>
             <div className="bg-white rounded-[28px] shadow-sm border border-slate-100 p-6 mb-8">
